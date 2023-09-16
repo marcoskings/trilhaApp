9cd5af42428e5593dddba1077ea28ae84808b2de
darkfrime <mr3841623@gmail.com>
2023-09-16 10:30:48 -0300

    Navegando para HomePage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/pages/main_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -145,11 +146,7 @@ class _LoginPageState extends State<LoginPage> {
                     onPressed: () {
                       if (_emailController.text.trim() == "email@email" &&
                           _senhaController.text.trim() == "123") {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text("Login efetuado com sucesso"),
-                          ),
-                        );
+                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MainPage()));
                       } else {
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(
